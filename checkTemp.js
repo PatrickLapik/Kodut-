15cf2ed85cc0924b468661e6f7e2e46753d2bb6b
PatrickLapik <113598320+PatrickLapik@users.noreply.github.com>
2022-09-15 13:43:59 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,17 @@
+function checkTemp()
+{
+    let value;
+
+    if (value < 21)
+    {
+        Vas = -1
+    }
+    else if (value > 40)
+    {
+        Vas = 1
+    }
+    else is (value != 20 && 40)
+    {
+        Vas = 0
+    }
+}
