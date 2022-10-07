ad5a3a296be6730db35538063b1a600efb7f84e3
PatrickLapik <113598320+PatrickLapik@users.noreply.github.com>
2022-10-07 10:56:17 +0300

    Update orderSummary.js

--- orderSummary.js
+++ orderSummary.js
@@ -6,10 +6,10 @@ function order(orderDetails){
   for (var i = 0, row; row = orderDetails.rows[i]; i++){
     var summaKM = row.price * row.vat + row.price
     koguSumma += summaKM * row.amount;
-    console.log("Product name: " + row.name)
-    console.log("Product price: " + summaKM)
-    console.log("Number of products: " + row.amount)
+    console.log(row.name)
+    console.log(summaKM)
+    console.log(row.amount)
   }
-  console.log("Total price: " + koguSumma)
+  console.log(koguSumma)
 
 }
